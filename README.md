64add6bc04eae85dbbe09b17ff58655aecbfc9fe
António Azevedo <antonio.v.azevedo@gmail.com>
2022-04-02 23:23:56 +0100

    hey, look over there!

--- README.md
+++ README.md
@@ -4,3 +4,46 @@
 ‣ [Data race](https://riptutorial.com/c/example/2622/data-race)
 
 ‣ [Mutex Locks](https://www.thegeekstuff.com/2012/05/c-mutex-examples/)
+
+## Arguments
+- number_of_philosophers (1+)
+- time_to_die (in ms)
+- time_to_eat (in ms)
+- time_to_sleep (in ms)
+- number_of_times_each_philosopher_must_eat _(optional)_
+
+## Activities
+Philosophers alternate between:
+- eating
+- sleeping
+- thinking
+
+The simulation stops when all philosophers have eaten at least `number_of_times_each_philosopher_must_eat` times (or if this is not specified, it stops when a philosopher dies).
+
+## Structure
+### main struct
+- input data (arguments)
+- philosophers (array of `philosopher` structs)
+
+### philosopher struct
+- `int` number
+- `enum` state
+- `struct` *left_fork
+- `struct` *right_fork
+
+### fork struct
+- `enum` state
+- mutex
+
+## Input validation
+- `number_of_philosophers` must be >= 1
+- `time_to_die`, `time_to_eat` and `time_to_sleep` must be >= 0
+- `number_of_times_each_philosopher_must_eat` (optional) must be >= 0
+
+## Initialization
+- Create main struct
+- Create fork struct for each fork (same as philosopher count)
+	- init fork mutex
+- Create philosopher struct for each philosopher
+	- include pointer for left and right forks
+- Start a thread for each philosopher
--- play.c
+++ play.c
@@ -6,7 +6,7 @@
 /*   By: aazevedo <aazevedo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 01:02:45 by aazevedo          #+#    #+#             */
-/*   Updated: 2022/03/25 01:33:47 by aazevedo         ###   ########.fr       */
+/*   Updated: 2022/04/02 19:06:03 by aazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ void	*thread_main(void *ptr)
 	str = (char *) ptr;
 	time = (struct timeval*) malloc(sizeof(struct timeval));
 	gettimeofday(time, NULL);
-	sleep(3);
-	printf("%ld.%d\tHello from thread %s...\n", time->tv_sec, time->tv_usec, str);
+	sleep(2);
+	printf("%ld.%d\tHello from thread %s...\n",  time->tv_sec, time->tv_usec, str);
+	free(time);
 	return (NULL);
 }
 
@@ -42,10 +43,11 @@ int	main(void)
 	pthread_create(thread1, NULL, &thread_main, "1");
 	pthread_create(thread2, NULL, &thread_main, "2");
 	pthread_create(thread3, NULL, &thread_main, "3");
-
 	pthread_join(*thread1, NULL);
 	pthread_join(*thread2, NULL);
 	pthread_join(*thread3, NULL);
 	printf("ending!\n");
+	printf("\npid:\t%d\n", getpid());
+	sleep(5);
 	return (0);
 }
