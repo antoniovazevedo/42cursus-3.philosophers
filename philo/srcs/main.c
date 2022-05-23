af56878c34b2ffcbfabea1ffb1ebe50dda3c519b
António Azevedo <antonio.v.azevedo@gmail.com>
2022-05-23 20:09:28 +0100

    validation

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,11 +6,21 @@
 /*   By: aazevedo <aazevedo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 18:25:29 by aazevedo          #+#    #+#             */
-/*   Updated: 2022/05/23 20:06:14 by aazevedo         ###   ########.fr       */
+/*   Updated: 2022/05/23 20:08:43 by aazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+void	debug_main(t_main *main)
+{
+	printf("philo_count =\t %d\n", main->philo_count);
+	printf("time_to_die =\t %d\n", main->time_to_die);
+	printf("time_to_eat =\t %d\n", main->time_to_eat);
+	printf("time_to_sleep =\t %d\n", main->time_to_sleep);
+	if (main->number_of_times_each_philosopher_must_eat)
+		printf("number_of_times_each_philosopher_must_eat =\t %d\n", main->number_of_times_each_philosopher_must_eat);
+}
 
 int	main(int argc, char **argv)
 {
@@ -30,11 +40,7 @@ int	main(int argc, char **argv)
 		free(main);
 		return (1);
 	}
-	printf("philo_count =\t %d\n", main->philo_count);
-	printf("time_to_die =\t %d\n", main->time_to_die);
-	printf("time_to_eat =\t %d\n", main->time_to_eat);
-	printf("time_to_sleep =\t %d\n", main->time_to_sleep);
-	printf("number_of_times_each_philosopher_must_eat =\t %d\n", main->number_of_times_each_philosopher_must_eat);
+	debug_main(main);
 
 	free(main);
 	return (0);
