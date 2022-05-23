9557ad19d527d8710e4494f45a98f523d83ea269
António Azevedo <antonio.v.azevedo@gmail.com>
2022-05-23 23:13:19 +0100

    wip

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aazevedo <aazevedo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 18:25:29 by aazevedo          #+#    #+#             */
-/*   Updated: 2022/05/23 21:21:19 by aazevedo         ###   ########.fr       */
+/*   Updated: 2022/05/23 23:13:16 by aazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,32 @@ void	debug_philo(t_philo *philo)
 		printf("forks: L %d\t\t R X\n\n", philo->left_fork->nb);
 }
 
+void	free_forks(t_fork **list, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(list[i]);
+		i++;
+	}
+	free(list);
+}
+
+void	free_threads(pthread_t **list, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(list[i]);
+		i++;
+	}
+	free(list);
+}
+
 t_fork	**create_forks(int count)
 {
 	t_fork	**forks;
@@ -47,33 +73,43 @@ t_fork	**create_forks(int count)
 	while (i < count)
 	{
 		fork = (t_fork *)malloc(sizeof(t_fork));
+		if (!fork || pthread_mutex_init(&fork->mutex, NULL) != 0)
+		{
+			free_forks(forks, i);
+			return (NULL);
+		}
 		fork->nb = i;
 		fork->is_taken = 0;
-		pthread_mutex_init(&fork->mutex, NULL);
 		forks[i] = fork;
 		i++;
 	}
 	return (forks);
 }
 
+void	print_state_message(int nb, char *state)
+{
+	struct timeval	*time;
+
+	time = (struct timeval *)malloc(sizeof(struct timeval));
+	if (!time)
+		return ;
+	gettimeofday(time, NULL);
+	printf("%d %d %s\n", time->tv_usec, nb, state);
+	free(time);
+}
+
+// pthread_mutex_lock(&data->philo->mutex);
 void	*thread_main(void *ptr)
 {
 	t_main			*data;
-	struct timeval	*time;
 
-	data = (t_main *)ptr;
-	time = (struct timeval *)malloc(sizeof(struct timeval));
-	gettimeofday(time, NULL);
-	sleep(2);
-
-	pthread_mutex_lock(&data->philo->mutex);
-	printf("%ld.%d\tHello from philo %d!\n", time->tv_sec, time->tv_usec, data->philo->nb);
-	pthread_mutex_unlock(&data->philo->mutex);
-	free(time);
+	data = ptr;
+	sleep(1);
+	print_state_message(data->philo->nb, "is initialized");
 	return (NULL);
 }
 
-void	initialize_philo(t_params *params, int nb, t_fork **forks)
+pthread_t	*initialize_philo(t_params *params, int nb, t_fork **forks)
 {
 	t_main		*main;
 	t_philo		*philo;
@@ -88,23 +124,44 @@ void	initialize_philo(t_params *params, int nb, t_fork **forks)
 		philo->right_fork = NULL;
 	else if (nb + 1 < params->philo_count)
 		philo->right_fork = forks[nb + 1];
-
-	pthread_mutex_init(&philo->mutex, NULL);
+	// if (pthread_mutex_init(&philo->mutex, NULL) != 0)
+		return (NULL);
 	debug_philo(philo);
-
 	main = (t_main *)malloc(sizeof(t_main));
 	main->params = params;
 	main->philo = philo;
-	printf("starting thread for philo nb=%d\n", main->philo->nb);
 	thread = (pthread_t *)malloc(sizeof(pthread_t));
-	pthread_create(thread, NULL, &thread_main, &main);
+	pthread_create(thread, NULL, &thread_main, main);
+	return (thread);
+}
+
+int	create_threads(t_params *params, t_fork **forks)
+{
+	int			philo_i;
+	pthread_t	**threads;
+
+	philo_i = 0;
+	threads = (pthread_t **)malloc(sizeof(pthread_t) * params->philo_count);
+	while (philo_i < params->philo_count)
+	{
+		threads[philo_i] = initialize_philo(params, philo_i, forks);
+		if (!threads[philo_i])
+		{
+			free_threads(threads, philo_i);
+			return (philo_i);
+		}
+		philo_i++;
+	}
+	philo_i = 0;
+	while (philo_i < params->philo_count)
+		pthread_join(*(threads[philo_i++]), NULL);
+	return (params->philo_count);
 }
 
 int	main(int argc, char **argv)
 {
 	t_params	*params;
 	t_fork		**forks;
-	int			philo_i;
 
 	if (argc < 5 || argc > 6)
 	{
@@ -120,10 +177,20 @@ int	main(int argc, char **argv)
 	}
 	debug_params(params);
 	forks = create_forks(params->philo_count);
-	philo_i = -1;
-	while (philo_i < params->philo_count - 1)
-		initialize_philo(params, ++philo_i, forks);
-	sleep(5);
+	if (!forks)
+	{
+		printf("Error\n");
+		free(params);
+		return (1);
+	}
+	if (create_threads(params, forks) < params->philo_count)
+	{
+		printf("Error\n");
+		free(params);
+		free_forks(forks, params->philo_count);
+		return (1);
+	}
 	free(params);
+	free_forks(forks, params->philo_count);
 	return (0);
 }
